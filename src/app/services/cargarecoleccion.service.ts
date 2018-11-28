487b83514fd33a8b890b1f4fbe493f6aa8be6012
Nachitop <nachitopsk@gmail.com>
2018-11-27 18:09:57 -0700

    revision 27

--- src/app/services/cargarecoleccion.service.ts
+++ src/app/services/cargarecoleccion.service.ts
@@ -10,16 +10,16 @@ export class CargarecoleccionService {
   readonly URL_API="http://localhost:3000/api/cargarecoleccion";
   hacerCargaRecoleccion(carga:any){
     
-    return this.http.post(this.URL_API,carga, this.authService.getHeader())
+    return this.http.post(this.URL_API,carga, this.authService.getHeader());
   }
   obtenerCargaRecoleccion(empleado:string){
-    return this.http.get(this.URL_API+"/"+empleado,this.authService.getHeader())
+    return this.http.get(this.URL_API+"/"+empleado,this.authService.getHeader());
   }
   obtenerCargasRecoleccion(sucursal:string){
-    return this.http.get(this.URL_API+"/sucursal/"+sucursal,this.authService.getHeader())
+    return this.http.get(this.URL_API+"/sucursal/"+sucursal,this.authService.getHeader());
   }
 
   editCargasRecoleccion(_id:string){
-    return this.http.post(this.URL_API+"/actualizar/"+_id,{},this.authService.getHeader())
+    return this.http.post(this.URL_API+"/actualizar/"+_id,{},this.authService.getHeader());
   }
 }
